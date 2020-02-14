67a7e346caa4b9e9f0c5d2bd534c017429ed631e
Chris Zubak-Skees <chriszs@gmail.com>
2020-02-14 17:14:22 -0500

    Then download the doc list for each facility

--- transforms/list.sh
+++ transforms/list.sh
@@ -2,16 +2,23 @@
 
 set -euo pipefail
 
-START=0
-END=75000
-while [  $START -lt $end ]; do
-    echo $START
-    response=$(curl -s 'https://www.fcc.gov/search/api/?t=opif&q=&s='$START'&o=new&f=\[\{%22political_file_type%22:%22PA%22\},\{%22political_file_type%22:%22NCIA%22\}\]')
-    docs=$(echo $response | jq -r -c '.response.docs[]')
-    while IFS= read -r doc; do
-        id=$(echo $doc | jq -r '.id')
-        echo $doc > $1'/'$id'.json'
-    done <<< "$docs"
+for file in $1/*.json; do
+    id=$(echo $file | tr -dc '0-9')
+    echo $id
+    curl -s 'https://publicfiles.fcc.gov/api/manager/search/key/Political.json?entityId='$id > $2/$id.json
     sleep 2
-    let START=START+10
 done
+
+# START=0
+# END=75000
+# while [  $START -lt $end ]; do
+#     echo $START
+#     response=$(curl -s 'https://www.fcc.gov/search/api/?t=opif&q=&s='$START'&o=new&f=\[\{%22political_file_type%22:%22PA%22\},\{%22political_file_type%22:%22NCIA%22\}\]')
+#     docs=$(echo $response | jq -r -c '.response.docs[]')
+#     while IFS= read -r doc; do
+#         id=$(echo $doc | jq -r '.id')
+#         echo $doc > $1'/'$id'.json'
+#     done <<< "$docs"
+#     sleep 2
+#     let START=START+10
+# done
