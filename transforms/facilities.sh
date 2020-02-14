4a314f565230f895a6dba5b43552b41795727f3c
Chris Zubak-Skees <chriszs@gmail.com>
2020-02-14 17:14:03 -0500

    Write out facilities into JSON files

--- transforms/facilities.sh
+++ transforms/facilities.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-curl "https://publicfiles.fcc.gov/api/service/facility/search/*.json" >> $1/facilities.json
+set -euo pipefail
+
+response=$(curl -s "https://publicfiles.fcc.gov/api/service/facility/search/*.json")
+
+for type in tvFacility amFacility fmFacility cableSystems; do
+    facilities=$(echo $response | jq -r -c '.results.globalSearchResults.'$type'List[]')
+
+    while IFS= read -r facility; do
+        id=$(echo $facility | jq -r '.id')
+        echo $facility > $1'/'$id'.json'
+    done <<< "$facilities"
+done
