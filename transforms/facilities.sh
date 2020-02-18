0a127b95f1869c86c1725de8ba78c664b620c1cf
Chris Zubak-Skees <chriszs@gmail.com>
2020-02-18 12:20:33 -0500

    Iterate through cable systems separately to handle special casing

--- transforms/facilities.sh
+++ transforms/facilities.sh
@@ -4,11 +4,20 @@ set -euo pipefail
 
 response=$(curl -s "https://publicfiles.fcc.gov/api/service/facility/search/*.json")
 
-for type in tvFacility amFacility fmFacility cableSystems; do
-    facilities=$(echo $response | jq -r -c '.results.globalSearchResults.'$type'List[]')
+facilities
+for type in tv am fm; do
+    facilities=$(echo $response | jq -r -c '.results.globalSearchResults.'$type'FacilityList[]')
 
     while IFS= read -r facility; do
         id=$(echo $facility | jq -r '.id')
         echo $facility > $1'/'$id'.json'
     done <<< "$facilities"
 done
+
+# cable systems
+facilities=$(echo $response | jq -r -c '.results.globalSearchResults.cableSystemsList[]')
+
+while IFS= read -r facility; do
+    id=$(echo $facility | jq -r '.psid')
+    echo $facility > $1'/'$id'.json'
+done <<< "$facilities"
